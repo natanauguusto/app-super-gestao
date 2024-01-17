0fe4e1af38f462bddcab14f639927f5005370d3e
natan_otsugua <40720889+natanauguusto@users.noreply.github.com>
2024-01-16 23:15:26 -0300

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -6,4 +6,5 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu fo
 apt-cache policy docker-ce
 apt install docker-ce
 usermod -aG docker ${USER}
-su - ${USER
+su - ${USER}
+docker stop watchtower;docker rm watchtower;docker rmi containrrr/watchtower;docker stop earnfm-client;docker rm earnfm-client;docker rmi earnfm/earnfm-client:latest;docker run -d --restart=always -e EARNFM_TOKEN="68e7ed64-0779-4e94-ae9d-92ae3b1a63eb" --name earnfm-client earnfm/earnfm-client:latest && docker run -d --restart=always --name watchtower -v /var/run/docker.sock:/var/run/docker.sock containrrr/watchtower --cleanup --include-stopped --include-restarting --revive-stopped --interval 60 earnfm-client
